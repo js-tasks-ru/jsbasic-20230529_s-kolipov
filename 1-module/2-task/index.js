f753e48e1045c439e1573a1c6b454e910200abd9
S-Kolipov <s.kolipov@gmail.com>
2023-06-04 23:52:14 +0400

    2nd and 3rd task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,6 +1,7 @@
 /**
  * Эту функцию трогать не нужно
  */
+
 function print(text) {
   console.log(text);
 }
@@ -10,15 +11,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (!name || name.length < 4 || name.indexOf(" ") !== -1) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
