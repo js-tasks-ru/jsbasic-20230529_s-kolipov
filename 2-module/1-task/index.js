f0dd653547a213a38abb86510264afb3e8c92c7e
S-Kolipov <s.kolipov@gmail.com>
2023-06-09 00:15:33 +0400

    Completed tasks 1 to 3 in the second module.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let salaryTotal = 0; // переменная для вывода суммы
+
+  for (let key in salaries) {
+    if (!isFinite(salaries[key])) {
+      // проверяем что в объекте нет спец числовых типов
+      return salaryTotal; // если да то выводим дефолтный ноль
+    } else if (typeof salaries[key] == "number") {
+      salaryTotal += salaries[key]; // если в объекте нет сец числовых, и если ест числа то складываем
+    }
+  }
+  return salaryTotal; // если пустой объект или без чисел то выведем дефолтный ноль
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,26 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.argA = a;
+    this.argB = b;
+  },
+  sum() {
+    let calcOut;
+    calcOut = this.argA + this.argB;
+    this.calcOut = calcOut;
+    return this.calcOut;
+  },
+  mul() {
+    let calcOut;
+    calcOut = this.argA * this.argB;
+    this.calcOut = calcOut;
+    return this.calcOut;
+  },
 };
 
+/*
+calculator.read(1, 0);
+console.log("Sum is", calculator.sum());
+console.log("Multiplauer is", calculator.mul());
+*/
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
